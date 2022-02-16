c69cbfe1d104744988b3855a61be9f8494ddb05c
kelvin589 <190081735@aston.ac.uk>
2022-02-16 22:44:26 +0000

    Add font picker
    Change font family and font size

--- lib/ui/widgets/add_message_popup.dart
+++ lib/ui/widgets/add_message_popup.dart
@@ -1,11 +1,16 @@
 import 'package:diginote/core/models/messages_model.dart';
 import 'package:diginote/core/providers/firebase_preview_provider.dart';
+import 'package:diginote/ui/shared/icon_helper.dart';
 import 'package:diginote/ui/shared/input_validators.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 
+import 'font_picker.dart';
+
 class AddMessagePopup extends StatefulWidget {
-  const AddMessagePopup({Key? key, required this.screenToken}) : super(key: key);
+  const AddMessagePopup({Key? key, required this.screenToken})
+      : super(key: key);
 
   final String screenToken;
 
@@ -18,28 +23,87 @@ class _AddMessagePopupState extends State<AddMessagePopup> {
   final TextEditingController _headerController = TextEditingController();
   final TextEditingController _messageController = TextEditingController();
 
+  String fontFamily = "Roboto";
+  double fontSize = 16.0;
+
   @override
   Widget build(BuildContext context) {
+    List<Widget> formOptions = [
+      TextFormField(
+        controller: _headerController,
+        decoration: const InputDecoration(hintText: 'Header'),
+        validator: isEmptyValidator,
+      ),
+      TextFormField(
+        keyboardType: TextInputType.multiline,
+        minLines: 5,
+        maxLines: 10,
+        controller: _messageController,
+        decoration: const InputDecoration(hintText: 'Message'),
+        validator: isEmptyValidator,
+        style: GoogleFonts.getFont(fontFamily, fontSize: fontSize),
+      ),
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text('Typeface'),
+          typeface,
+        ],
+      ),
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text('Font'),
+          FontPicker(
+            onFontFamilyChanged: (fontFamily) {
+              setState(() {
+                this.fontFamily = fontFamily;
+              });
+            },
+            onFontSizeChanged: (fontSize) {
+              setState(() {
+                this.fontSize = fontSize;
+              });
+            },
+          ),
+        ],
+      ),
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: const [
+          Text('Font Colour'),
+        ],
+      ),
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: const [
+          Text('Background Colour'),
+        ],
+      ),
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: const [
+          Text('Listing'),
+        ],
+      ),
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: const [
+          Text('Text Alignment'),
+        ],
+      ),
+    ];
+
     return AlertDialog(
       title: const Text('Add Message'),
       content: Form(
         key: _formKey,
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text('Enter the message to display:'),
-            TextFormField(
-              controller: _headerController,
-              decoration: const InputDecoration(hintText: 'Header'),
-              validator: isEmptyValidator,
-            ),
-            TextFormField(
-              controller: _messageController,
-              decoration: const InputDecoration(hintText: 'Message'),
-              validator: isEmptyValidator,
-            ),
-          ],
+        child: ListView.separated(
+          itemCount: formOptions.length,
+          separatorBuilder: (context, index) => const Divider(
+            color: Colors.transparent,
+          ),
+          itemBuilder: (context, index) => formOptions[index],
         ),
       ),
       actions: [
@@ -66,10 +130,37 @@ class _AddMessagePopupState extends State<AddMessagePopup> {
   }
 
   void _okPressed() {
-    Message newMessage = Message(header: _headerController.text, message: _messageController.text);
+    Message newMessage = Message(
+        header: _headerController.text, message: _messageController.text);
     if (_formKey.currentState!.validate()) {
-      Provider.of<FirebasePreviewProvider>(context, listen: false).addMessage(widget.screenToken, newMessage);
+      Provider.of<FirebasePreviewProvider>(context, listen: false)
+          .addMessage(widget.screenToken, newMessage);
       Navigator.pop(context);
     }
   }
+
+  Widget typeface = Row(
+    children: [
+      IconButton(
+        onPressed: () => {},
+        icon: IconHelper.boldIcon,
+        constraints: const BoxConstraints(),
+      ),
+      IconButton(
+        onPressed: () => {},
+        icon: IconHelper.italicIcon,
+        constraints: const BoxConstraints(),
+      ),
+      IconButton(
+        onPressed: () => {},
+        icon: IconHelper.strikethroughIcon,
+        constraints: const BoxConstraints(),
+      ),
+      IconButton(
+        onPressed: () => {},
+        icon: IconHelper.underlineIcon,
+        constraints: const BoxConstraints(),
+      ),
+    ],
+  );
 }
