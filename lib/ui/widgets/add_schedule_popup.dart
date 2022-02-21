c8d9454217ccb7bcecba714fe5a16f257e339fea
kelvin589 <190081735@aston.ac.uk>
2022-02-21 22:27:06 +0000

    Add quick options to scheduling
    Allow user to schedule messages from preset options
    No scheduling occurs when from and to are equal
    Remove inset padding from popup to make it larger

--- lib/ui/widgets/add_schedule_popup.dart
+++ lib/ui/widgets/add_schedule_popup.dart
@@ -25,12 +25,39 @@ class _AddSchedulePopupState extends State<AddSchedulePopup> {
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
+      insetPadding: EdgeInsets.zero,
       title: const Text('Set Schedule'),
       content: Form(
         child: Column(
           mainAxisSize: MainAxisSize.min,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            const Text("Quick Options"),
+            Row(
+              children: [
+                GestureDetector(
+                  onTap: () => setState(() {
+                    fromDate = DateTime.now();
+                    fromTime = TimeOfDay.now();
+                    toDate = DateTime.now();
+                    toTime = TimeOfDay.now();
+                  }),
+                  child: const _QuickItem(text: "None"),
+                ),
+                GestureDetector(
+                  onTap: () => _setQuickOptions(setMinutes: 5),
+                  child: const _QuickItem(text: "5 Min"),
+                ),
+                GestureDetector(
+                  onTap: () => _setQuickOptions(setMinutes: 10),
+                  child: const _QuickItem(text: "10 Min"),
+                ),
+                GestureDetector(
+                  onTap: () => _setQuickOptions(setMinutes: 15),
+                  child: const _QuickItem(text: "15 Min"),
+                ),
+              ],
+            ),
             const Text("From"),
             Row(
               children: [
@@ -139,17 +166,31 @@ class _AddSchedulePopupState extends State<AddSchedulePopup> {
     }
   }
 
+  void _setQuickOptions({required int setMinutes}) {
+    DateTime dateTimeNow = DateTime.now();
+    TimeOfDay timeOfDayNow = TimeOfDay.now();
+
+    setState(() {
+      fromDate = dateTimeNow;
+      fromTime = timeOfDayNow;
+      toDate = dateTimeNow;
+      toTime = timeOfDayNow.replacing(minute: timeOfDayNow.minute + setMinutes);
+    });
+  }
+
   void _cancelPressed() {
     Navigator.pop(context);
   }
 
   void _okPressed() {
-    DateTime from = DateTime(fromDate.year, fromDate.month, fromDate.day, fromTime.hour, fromTime.minute);
-    DateTime to = DateTime(toDate.year, toDate.month, toDate.day, toTime.hour, toTime.minute);
+    DateTime from = DateTime(fromDate.year, fromDate.month, fromDate.day,
+        fromTime.hour, fromTime.minute);
+    DateTime to = DateTime(
+        toDate.year, toDate.month, toDate.day, toTime.hour, toTime.minute);
 
     Provider.of<FirebasePreviewProvider>(context, listen: false)
         .updateMessageSchedule(widget.screenToken, widget.message, from, to);
-    
+
     Navigator.pop(context);
   }
 }
@@ -194,6 +235,26 @@ class _TimeItem extends StatelessWidget {
         child: Text(
           time.format(context),
         ),
+      ),
+    );
+  }
+}
+
+class _QuickItem extends StatelessWidget {
+  const _QuickItem({Key? key, required this.text}) : super(key: key);
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(right: 8.0, bottom: 8.0),
+      child: Container(
+        padding: const EdgeInsets.all(3),
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.black),
+        ),
+        child: Text(text),
       ),
     );
   }
