603f853754ad05b76bd388e3f7177a4e85e3296e
kelvin589 <190081735@aston.ac.uk>
2022-03-03 21:24:30 +0000

    Test home view
    Ensure that bottom bar buttons lead to the correct view

---
+++ test/ui/views/home_view_test.dart
@@ -0,0 +1,52 @@
+import 'package:diginote/core/providers/firebase_screens_provider.dart';
+import 'package:diginote/ui/views/home_view.dart';
+import 'package:diginote/ui/views/screens_view.dart';
+import 'package:diginote/ui/views/settings_view.dart';
+import 'package:diginote/ui/views/templates_view.dart';
+import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
+import 'package:firebase_auth_mocks/firebase_auth_mocks.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:provider/provider.dart';
+
+void main() async {
+  late FakeFirebaseFirestore firestoreInstance;
+  late MockFirebaseAuth firebaseAuth;
+  late FirebaseScreensProvider screensProvider;
+  MockUser mockUser = MockUser();
+
+  setUp(() {
+    firestoreInstance = FakeFirebaseFirestore();
+    firebaseAuth = MockFirebaseAuth(signedIn: true, mockUser: mockUser);
+    screensProvider = FirebaseScreensProvider(
+        firestoreInstance: firestoreInstance, authInstance: firebaseAuth);
+  });
+
+  Future<void> loadHomeView(WidgetTester tester) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        home: ChangeNotifierProvider<FirebaseScreensProvider>(
+          create: (_) => screensProvider,
+          child: const HomeView(),
+        ),
+      ),
+    );
+  }
+
+  testWidgets('Test widget buttons work correctly',
+      (WidgetTester tester) async {
+    await loadHomeView(tester);
+
+    await tester.tap(find.text('Templates'));
+    await tester.pumpAndSettle();
+    expect(find.byType(TemplatesView), findsOneWidget);
+
+    await tester.tap(find.text('Settings'));
+    await tester.pumpAndSettle();
+    expect(find.byType(SettingsView), findsOneWidget);
+
+    await tester.tap(find.text('Screens'));
+    await tester.pumpAndSettle();
+    expect(find.byType(ScreensView), findsOneWidget);
+  });
+}
