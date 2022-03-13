884217d52ba8fe759d2d005b1da77190238f5e2d
kelvin589 <190081735@aston.ac.uk>
2022-03-13 21:59:50 +0000

    Show battery percentage
    Separate screen item
    Rename old screen item to screen item content
    Separate to update only the required screen item when battery percentage changes

--- lib/ui/views/screens_view.dart
+++ lib/ui/views/screens_view.dart
@@ -1,10 +1,6 @@
 import 'package:diginote/core/models/screen_model.dart';
 import 'package:diginote/core/providers/firebase_screens_provider.dart';
-import 'package:diginote/core/providers/zoom_provider.dart';
-import 'package:diginote/ui/shared/timer_provider.dart';
-import 'package:diginote/ui/views/preview_view.dart';
 import 'package:diginote/ui/widgets/screen_item.dart';
-import 'package:diginote/ui/widgets/screen_settings_popup.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -27,8 +23,13 @@ class ScreensView extends StatelessWidget {
 
         Iterable<Screen>? screens = snapshot.data;
         if (screens != null) {
-          List<Widget> items = <Widget>[];
-          items = _updateScreenItems(context, screens);
+          List<Widget> items = screens
+              .map(
+                (screen) => ScreenItem(
+                  screen: screen,
+                ),
+              )
+              .toList();
           return ListView.builder(
             padding: const EdgeInsets.only(top: 8),
             itemCount: items.length,
@@ -46,77 +47,5 @@ class ScreensView extends StatelessWidget {
         }
       },
     );
-  }
-
-  List<Widget> _updateScreenItems(
-      BuildContext context, Iterable<Screen>? screens) {
-    // TODO: Add battery percentage
-    const batteryPercentage = 100;
-
-    if (screens != null) {
-      return screens
-          .map(
-            (screen) => ScreenItem(
-              screenName: screen.name,
-              lastUpdated: screen.lastUpdated,
-              batteryPercentage: batteryPercentage,
-              onPreviewTapped: () => _showPreview(context, screen),
-              onSettingsTapped: () => _showScreenSettings(
-                context: context,
-                screenToken: screen.screenToken,
-                screenName: screen.name,
-              ),
-            ),
-          )
-          .toList();
-    }
-    return [];
-  }
-
-  void _showPreview(BuildContext context, Screen screen) {
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (context) => MultiProvider(
-          providers: [
-            ChangeNotifierProvider<TimerProvider>(
-              create: (context) =>
-                  TimerProvider(duration: const Duration(seconds: 1)),
-            ),
-            ChangeNotifierProvider<ZoomProvider>(
-              create: (context) =>
-                  ZoomProvider(),
-            ),
-          ],
-          child: PreviewView(
-            screenToken: screen.screenToken,
-            screenWidth: screen.width,
-            screenHeight: screen.height,
-            screenName: screen.name,
-          ),
-        ),
-      ),
-    );
-  }
-
-  void _showScreenSettings(
-      {required BuildContext context,
-      required String screenToken,
-      required String screenName}) {
-    showDialog(
-      context: context,
-      builder: (context) => ScreenSettingsPopup(
-        screenToken: screenToken,
-        screenName: screenName,
-        onDelete: () async {
-          await _onDelete(context, screenToken);
-        },
-      ),
-    );
-  }
-
-  Future<void> _onDelete(BuildContext context, String screenToken) async {
-    await Provider.of<FirebaseScreensProvider>(context, listen: false)
-        .deleteScreen(screenToken);
   }
 }
---
+++ lib/ui/widgets/screen_item_content.dart
@@ -0,0 +1,58 @@
+import 'package:diginote/ui/shared/icon_helper.dart';
+import 'package:flutter/material.dart';
+
+class ScreenItemContent extends StatelessWidget {
+  const ScreenItemContent(
+      {Key? key,
+      required this.screenName,
+      required this.lastUpdated,
+      required this.batteryPercentage,
+      required this.onSettingsTapped,
+      required this.onPreviewTapped})
+      : super(key: key);
+
+  final String screenName;
+  final DateTime lastUpdated;
+  final int batteryPercentage;
+  final Function() onSettingsTapped;
+  final Function() onPreviewTapped;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(screenName),
+      subtitle: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text('Last Updated: ${lastUpdatedString(lastUpdated)}'),
+          Text('Battery Percentage: $batteryPercentage%'),
+        ],
+      ),
+      trailing: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          IconButton(
+            onPressed: onSettingsTapped,
+            icon: IconHelper.settingsIcon,
+            constraints: const BoxConstraints(),
+          ),
+          const SizedBox(width: 10),
+          IconButton(
+            onPressed: onPreviewTapped,
+            icon: IconHelper.previewIcon,
+            constraints: const BoxConstraints(),
+          ),
+        ],
+      ),
+    );
+  }
+
+  String lastUpdatedString(DateTime lastUpdated) {
+    String day = lastUpdated.day.toString().padLeft(2, '0');
+    String month = lastUpdated.month.toString().padLeft(2, '0');
+    int year = lastUpdated.year;
+    String hour = lastUpdated.hour.toString().padLeft(2, '0');
+    String minute = lastUpdated.minute.toString().padLeft(2, '0');
+    return "$day/$month/$year - $hour:$minute";
+  }
+}
