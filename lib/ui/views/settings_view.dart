822d42adaf198f0808c45335d91db312901ca088
kelvin589 <190081735@aston.ac.uk>
2022-03-19 14:50:51 +0000

    Add settings options placeholders
    Set logout button to red

--- lib/ui/views/settings_view.dart
+++ lib/ui/views/settings_view.dart
@@ -1,18 +1,61 @@
 import 'package:diginote/core/providers/firebase_login_provider.dart';
+import 'package:diginote/ui/widgets/colour_picker_icon.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-class SettingsView extends StatelessWidget {
+class SettingsView extends StatefulWidget {
   const SettingsView({Key? key}) : super(key: key);
 
   @override
+  State<SettingsView> createState() => _SettingsViewState();
+}
+
+class _SettingsViewState extends State<SettingsView> {
+  bool isDarkMode = false;
+  bool allowNotifications = false;
+
+  @override
   Widget build(BuildContext context) {
-    return Center(
-      child: ElevatedButton(
-        onPressed: () async {
-          await Provider.of<FirebaseLoginProvider>(context, listen: false).logout();
-        },
-        child: const Text("Logout"),
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const Text("Allow Notifications"),
+            Switch(
+              value: allowNotifications,
+              onChanged: (newValue) => setState(
+                () {
+                  allowNotifications = newValue;
+                },
+              ),
+            ),
+            const Text("Dark Mode"),
+            Switch(
+              value: isDarkMode,
+              onChanged: (newValue) => setState(
+                () {
+                  isDarkMode = newValue;
+                },
+              ),
+            ),
+            const Text("Background Colour"),
+            ColourPickerIcon(onColourChanged: (newColour) => {}),
+            const Text("Foreground Colour"),
+            ColourPickerIcon(onColourChanged: (newColour) => {}),
+            ElevatedButton(
+              onPressed: () async {
+                await Provider.of<FirebaseLoginProvider>(context, listen: false)
+                    .logout();
+              },
+              child: const Text("Logout"),
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Colors.red),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
