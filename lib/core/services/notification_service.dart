fcff3bbce5a42be7df4e63c9ce94057c25116351
kelvin589 <190081735@aston.ac.uk>
2022-04-11 18:27:19 +0100

    Add comments for services

--- lib/core/services/notification_service.dart
+++ lib/core/services/notification_service.dart
@@ -2,10 +2,16 @@ import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
+/// A service to manage notifications by initialising the notifications for
+/// supported platforms.
+/// 
+/// The [init] method must be called to initialise [NotificationService].
 class NotificationService {
+  /// The [FlutterLocalNotificationsPlugin] instance.
   final FlutterLocalNotificationsPlugin localNotifications =
       FlutterLocalNotificationsPlugin();
 
+  /// Initialises [FlutterLocalNotificationsPlugin].
   Future<void> init() async {
     const AndroidInitializationSettings initializationSettingsAndroid =
         AndroidInitializationSettings('app_icon');
@@ -25,15 +31,18 @@ class NotificationService {
 
     await localNotifications.initialize(initializationSettings);
 
+    // Listen to incoming FCM payloads to handle foreground notifications.
     FirebaseMessaging.onMessage.listen((message) async {
       debugPrint("Received");
       await showMessage(message, localNotifications);
     });
 
+    // Handles notifications in the background.
     FirebaseMessaging.onBackgroundMessage(_backgroundMessageHandler);
   }
 }
 
+/// Displays the notification.
 Future<void> showMessage(RemoteMessage message,
     FlutterLocalNotificationsPlugin localNotifications) async {
   const AndroidNotificationDetails notificationDetailsAndroid =
@@ -54,6 +63,9 @@ Future<void> showMessage(RemoteMessage message,
   );
 }
 
+/// The handler for background messages.
+///
+/// [FirebaseMessaging.onBackgroundMessage] handler must be a top-level function. 
 Future<void> _backgroundMessageHandler(RemoteMessage message) async {
   debugPrint("received in background");
 }
